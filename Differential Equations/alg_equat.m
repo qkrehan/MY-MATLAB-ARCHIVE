8e80cbbe2006600e0e5850582fd43c688a8ae59e
Rehan Qasim <qkrehan@gmail.com>
2019-06-21 23:04:32 +0500

    Update alg_equat.m

--- Differential Equations/alg_equat.m
+++ Differential Equations/alg_equat.m
@@ -1,29 +1,41 @@
-%{
+%% 
 syms x
 eqn = sin(x) == 1;
 solx = solve(eqn,x)
-%} 
+ 
+%% 
 syms x
 eqn = x^2+2*x+4 == 0;
 solx = solve(eqn,x)
+
+%% solving  
 
 syms y x 
 y = 2*x^4 + 1;
 diff(y,2)
 
-syms x y z
-eq1 = x +3*y +z == 0
-eq2 = x +3*y + 5*z == 0
-eq3 = 2*x + 5*y +z == 0
-solve([eq1,eq2,eq3],[x y z])
+%%
+
+syms u v
+eqns = [2*u + v == 0, u - v == 1];
+s = solve(eqns, [u v])
 
 
 
+%% 
 
+syms x
+eqns = [3*x+2, 3*x+1];
+solve(eqns, x)
 
+%% 
 
+solve(x + 1 == 5, x)
 
-
-
-
+%%
+syms x y 
+eqns01 = input('Input the first equation :')
+eqns02 = input('Input the Second equation :')
+eqns = [eqns01, eqns02];
+[u,v]=solve(eqns)
 
