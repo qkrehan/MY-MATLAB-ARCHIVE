093fc93c66a09304d1a88e8b9724e432d3a89739
Rehan Qasim <qkrehan@gmail.com>
2019-06-24 15:59:26 +0500

    24062019

---
+++ fcs labs/1/tf_ilplce.m
@@ -0,0 +1,24 @@
+%%
+x  = [1 2 3 4 5 6 7 8 9];
+y  = [7 8 9 4 5 6 1 2 3];
+fn1 = tf(x,y)
+%%
+syms x y
+f = 1/sqrt(x);
+laplace(f)
+%%
+syms s x
+x = ilaplace(1/s)
+y = laplace(x)
+
+%%
+q = ilaplace(sym(1))
+class(q)
+
+%%
+x = 7;
+
+isvarname('double')
+who
+%%
+isvarname('double')
